a39058ca0601fbe0bb28ed92405037ad2526ca4f
Willian Alves <wba.alves@gmail.com>
2019-06-12 18:40:44 -0300

    use synchronizer to start all processes

--- app.py
+++ app.py
@@ -4,35 +4,49 @@ import random
 
 from node import Node
 from counters.counter import Counter
+from threads.synchronizer import Synchronizer
+from helpers.process_helper import get_process_parameters
+from helpers.threads_helper import create_synchronizer_thread
 
 current_node = None
 config_file = sys.argv[1]
 processes = open(config_file)
 nodes = {}
+last_node_id = None
 events = Counter()
 
 
 def main():
     create_nodes()
+    synchronize()
     current_node.start_receiver()
     run_events()
 
 
 def create_nodes():
     global current_node
+    global last_node_id
     for line in processes:
         process_id, process_address = get_process_parameters(line)
         if process_id == sys.argv[2]:
             current_node = Node(process_id, process_address, events)
         else:
             nodes[process_id] = Node(process_id, process_address, events)
+        last_node_id = process_id
 
 
-def get_process_parameters(process_line):
-    process = process_line.split(" ")
-    process_id = process[0]
-    process_address = process[1], int(process[2])
-    return process_id, process_address
+def synchronize():
+    synchronizer = create_synchronizer_thread(current_node.pid, current_node.address, get_process_list())
+    if (sys.argv[2] == last_node_id):
+        print("Sending signal to all processes.")
+        synchronizer.send_signal()
+    else:
+        print("Waiting signal from last process.")
+        synchronizer.receive_signal()
+
+
+def get_process_list():
+    return processes.readlines()
 
 
 def run_events():
@@ -41,7 +55,7 @@ def run_events():
         if is_local():
             current_node.increment_local()
             increment_event()
-        elif events.read() > 5:
+        else:
             send_clock_value()   
             confirm_value_receipt()
             increment_event()
